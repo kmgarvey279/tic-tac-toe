7ac45af8166393c220fc9cd685b121f4b08c4fb8
Crystal Fecteau and Kevin Garvey <crystal@>
2019-04-10 16:26:45 -0700

    clean code and add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,13 @@
+// Game Object Logic
 function Game() {
 
 }
 
+// Board Object Logic
 function Board() {
   this.playerOneSpaces = [];
   this.playerTwoSpaces = [];
 }
-
-// var win1 = \0,1,2\
-// var win2 = \0,4,8\
-// var win3 = \0,3,6\
-// var win4 = \1,4,7\
-// var win5 = \2,4,6\
-// var win6 = \2,5,8\
-// var win7 = \3,4,5\
-// var win8 = \6,7,8\
-//
-// if (win1 || win2 || win3 || win4 || win5 || win6 || win7 || win8) {
-//
-// }
-
-
-
-
 
 Board.prototype.addPlayerOneSpace = function(playerOneSpace) {
   this.playerOneSpaces.push(playerOneSpace);
@@ -32,52 +17,42 @@ Board.prototype.addPlayerTwoSpace = function(playerTwoSpace) {
   this.playerTwoSpaces.push(playerTwoSpace);
 }
 
+// Space Object Logic
 
 function Space(spaceId) {
   this.spaceId = spaceId;
 }
 
-// Space.prototype.playerClick() {
-// }
-
 function Player(selectOX) {
 this.selectOX = selectOX;
 }
 
-var playerOne = new Player();
-var playerTwo = new Player();
-
 
 // User Interface Logic
+var playerOne;
+var playerTwo;
 var newGame = new Game();
 var newBoard = new Board();
 
+function userAssignment() {
+  var userOneAssignment = $("input:radio[name=select-x-o]:checked").val();
+  if (userOneAssignment === "o") {
+    var userTwoAssignment = "x";
+  } else if (userOneAssignment === "x") {
+    var userTwoAssignment = "o";
+  }
+  playerOne = new Player(userOneAssignment);
+  playerTwo = new Player(userTwoAssignment);
+}
 
 
-
-// function changeSpace(id) {
-//   var space = newBoard.findSpace(id);
-//   for ()
-//
-// }
-
-var userOneAssignment;
-var playerOne;
-var playerTwo;
-
+var clickCount = 0;
 $(document).ready(function(){
   $(".userSelect").submit(function(event){
     event.preventDefault();
-    var userOneAssignment = $("input:radio[name=select-x-o]:checked").val();
-    if (userOneAssignment === "o") {
-      var userTwoAssignment = "x";
-    } else if (userOneAssignment === "x") {
-      var userTwoAssignment = "o";
-    }
-    playerOne = new Player(userOneAssignment);
-    playerTwo = new Player(userTwoAssignment);
+    userAssignment();
   });
-var clickCount = 0;
+
   $(".grid-content").click(function(event) {
     var clickedSpace = $(this);
     var getId = $(this).attr("id");
@@ -94,7 +69,7 @@ var clickCount = 0;
         newBoard.addPlayerOneSpace(newSpace);
       }
       // alert(newBoard.playerOneSpaces[0])
-      var win1 = /[012][012][012]/
+      // var win1 = /[012][012][012]/
       // var win2 = /0,4,8/
       // var win3 = /0,3,6/
       // var win4 = /1,4,7/
@@ -102,14 +77,14 @@ var clickCount = 0;
       // var win6 = /2,5,8/
       // var win7 = /3,4,5/
       // var win8 = /6,7,8/
-      if (clickCount % 2 === 0) {
-        for (var i = 0; i < newBoard.playerOneSpaces.length; i++) {
-          console.log(newBoard.playerOneSpaces[i]);
-      }
-      } else {
-      for (var i = 0; i < newBoard.playerTwoSpaces.length; i++) {
-      console.log(newBoard.playerTwoSpaces[i]);
-      }
-    }
+    //   if (clickCount % 2 === 0) {
+    //     for (var i = 0; i < newBoard.playerOneSpaces.length; i++) {
+    //       console.log(newBoard.playerOneSpaces[i]);
+    //   }
+    //   } else {
+    //   for (var i = 0; i < newBoard.playerTwoSpaces.length; i++) {
+    //   console.log(newBoard.playerTwoSpaces[i]);
+    //   }
+    // }
   });
 });
